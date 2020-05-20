2aa9dd25333300c21da9454082c6c8f22ec94195
ZEROUALI Sara <s.zerouali@cfa-insta.fr>
2020-05-20 15:10:48 +0200

    blog

---
+++ front-react/src/router/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Footer(props) {
+    return (
+        <div className="footer">
+            <h2>Footer</h2>
+        </div>
+    );
+}
+  
---
+++ front-react/src/router/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header(props) {
+    return (
+        <div className="header">
+            <img src="logo-cfa-insta.svg"></img>
+        </div>
+    );
+}
+  
